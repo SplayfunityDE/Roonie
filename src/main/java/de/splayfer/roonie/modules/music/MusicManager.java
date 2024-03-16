1f148d9620b476745542d9dff5132a23fbc2b9ea
Splayfery <splayfery@gmail.com>
2024-03-17 00:56:43 +0100

    music system hotfix

--- src/main/java/de/splayfer/roonie/modules/music/MusicManager.java
+++ src/main/java/de/splayfer/roonie/modules/music/MusicManager.java
@@ -1,6 +1,8 @@
 package de.splayfer.roonie.modules.music;
 
 import de.splayfer.roonie.Roonie;
+import de.splayfer.roonie.modules.music.commands.PlayCommand;
+import de.splayfer.roonie.modules.music.commands.StopCommand;
 import de.splayfer.roonie.modules.poll.PollCreateCommand;
 import de.splayfer.roonie.modules.poll.PollEnterListener;
 import de.splayfer.roonie.utils.CommandManager;
@@ -11,13 +13,14 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 public class MusicManager {
 
     public static void init() {
-        Roonie.builder.addEventListeners(new PollCreateCommand(), new PollEnterListener());
+        Roonie.builder.addEventListeners(new PlayCommand(), new StopCommand());
 
         CommandManager.addCommands(Guilds.MAIN,
                 Commands.slash("play", "▶ │ Spiele Inhalte von YouTube, Spotify, Soundcloud, Twitch und vielem mehr!")
-                        .addOption(OptionType.STRING, "inhalt", "� │ Inhalt, welchen du wiedergeben möchtest!")
+                        .addOption(OptionType.STRING, "inhalt", "� │ Inhalt, welchen du wiedergeben möchtest!", true)
                         .addOption(OptionType.BOOLEAN, "warteschlange", "� │ Gib, ob der Inhalt in die Warteschlange hinzugeügt werden soll!", false),
                 Commands.slash("pause", "⏸ │ Pausiert den aktuell gespielten Inhalt!"),
+                Commands.slash("resume", "⏯ │ Setzt den aktuellen Inhalt fort!"),
                 Commands.slash("stop", "⏹ │ Stoppt den aktuell gespielten Inhalt!"),
                 Commands.slash("skip", "⏯ │ Übersprint den aktuellen Titel der Warteschleife!"),
                 Commands.slash("shuffle", "� │ Würfelt die aktuellen Titel der Warteschleifen durcheinander!"));
