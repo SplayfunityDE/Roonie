82c321a04d01c80d257d8d59f167690cac682e47
Splayfery <splayfery@gmail.com>
2024-04-08 23:43:39 +0200

    fix TicketRestoreListener

--- src/main/java/de/splayfer/roonie/modules/ticket/TicketRestoreListener.java
+++ src/main/java/de/splayfer/roonie/modules/ticket/TicketRestoreListener.java
@@ -13,17 +13,14 @@ public class TicketRestoreListener {
         for (Ticket ticket : list.keySet()) {
             //check for threads & post
             if (!(Roonie.mainGuild.getThreadChannels().contains(ticket.getChannel()) && Roonie.mainGuild.getForumChannelById(Channels.TICKETFORUM.getId()).getThreadChannels().contains(ticket.getPost()))) {
-                System.out.println("Ticket " + " gelöscht");
             } else if (!Roonie.mainGuild.getThreadChannels().contains(ticket.getChannel())) {
                 //check only for threads
                 ticket.prune(list.get(ticket));
-                System.out.println("Ticket " + " wegen fehlendem Channel gelöscht");
             } else if (!Roonie.mainGuild.getForumChannelById(Channels.TICKETFORUM.getId()).getThreadChannels().contains(ticket.getPost())) {
                 //check only for post
                 ticket.setPost(Roonie.mainGuild.getForumChannelById(Channels.TICKETFORUM.getId()).createForumPost(Ticket.typeSymbol.get(ticket.getType()) + "-" + ticket.getCreator().getEffectiveName(), MessageCreateData.fromContent("\u200E")).complete().getThreadChannel());
                 ticket.getPost().sendMessageEmbeds(ticket.getPostEmbed()).queue();
                 ticket.updateMongoDB();
-                System.out.println("Ticket " + " wegen fehlendem Post wiederhergestellt");
             }
             //check for member left - if ticket is unclaimed
             if (ticket.getSupporter() == null && !Roonie.mainGuild.getMembers().contains(ticket.getCreator())) {
@@ -31,7 +28,6 @@ public class TicketRestoreListener {
                     ticket.close("Server verlassen");
                 else
                     ticket.prune(list.get(ticket));
-                System.out.println("Server verlassen");
             }
         }
     }
