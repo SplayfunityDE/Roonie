811d5b1b39790a7568c08f16b6ed5b1643224b32
splayfery <simon.liedlc@gmail.com>
2024-04-29 11:09:58 +0200

    fix welcome dm message implementation

--- src/main/java/de/splayfer/roonie/general/WelcomeListener.java
+++ src/main/java/de/splayfer/roonie/general/WelcomeListener.java
@@ -1,6 +1,9 @@
 package de.splayfer.roonie.general;
 
+import de.splayfer.roonie.Roonie;
 import de.splayfer.roonie.general.schedule.BotCounter;
+import de.splayfer.roonie.utils.DefaultMessage;
+import de.splayfer.roonie.utils.enums.Channels;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
@@ -23,22 +26,22 @@ public class WelcomeListener extends ListenerAdapter {
             mainEmbed.setColor(0x28346d);
             mainEmbed.setThumbnail("https://cdn.discordapp.com/attachments/906251556637249547/938746351905288212/879274641070391306.gif");
             mainEmbed.setTitle("Willkommen auf SPLΛYFUNITY");
-            mainEmbed.setDescription("Hey " + event.getUser().getAsMention() + " herzlich <a:hallo:879274641070391306> willkommen!");
-            mainEmbed.addField("<:badgesupporter:875080765627568148> Wir bieten dir", "<:point:940963558693425172> Regelmäßige Events \n" +
-                    "<:point:940963558693425172> Zahlreiche Giveaways \n" +
+            mainEmbed.setDescription("> Hey " + event.getUser().getAsMention() + " herzlich <a:hallo:879274641070391306> willkommen");
+            mainEmbed.addField("Wir bieten dir", "<:point:940963558693425172> Regelmäßige Events \n" +
+                    "<:point:940963558693425172> Viele Giveaways \n" +
                     "<:point:940963558693425172> Coole Belohnungen \n" +
-                    "<:point:940963558693425172> Ein kompetentes Team \n", false);
-            mainEmbed.addField("<:level:909085962934562896> Regelmäßig coole Preise", "<:point:940963558693425172> Eigene Discord Bots \n" +
+                    "<:point:940963558693425172> Ein kompetentes Team \n", true);
+            mainEmbed.addField("Regelmäßig coole Preise", "<:point:940963558693425172> Eigene Discord Bots \n" +
                     "<:point:940963558693425172> Designs / Banner \n" +
                     "<:point:940963558693425172> Discord Nitro \n" +
-                    "<:point:940963558693425172> Steam Games", false);
-            mainEmbed.addField(":question: Du benötigst Hilfe?", "Dann wende dich an den Support in <#908795138623537232>. Dort kannst du einfach ein Ticket öffnen!", false);
+                    "<:point:940963558693425172> Steam Games", true);
+            mainEmbed.addField(":question: Du benötigst Hilfe?", "Teile uns dein Anliegen im `#ticket` Kanal mit!", false);
             mainEmbed.setImage("https://cdn.discordapp.com/attachments/906251556637249547/925055440436477982/auto_faqw.png");
 
             List<Button> buttons = new ArrayList<>();
-            buttons.add(Button.primary("welcome.visit", "Schau dich jetzt um!").withUrl("https://discord.gg/splayfer").withEmoji(Emoji.fromCustom("chat", Long.parseLong("880875728915275786"), true)));
-            buttons.add(Button.success("welcome.features", "Schau dir unsere Features an!").withEmoji(Emoji.fromCustom("level", Long.parseLong("909085962934562896"), false)));
-            buttons.add(Button.secondary("welcome.question", "Stelle eine Frage!").withEmoji(Emoji.fromFormatted("❓")));
+            buttons.add(Button.primary("welcome.visit", "Schau dich jetzt um!").withUrl("https://discord.gg/V2Vc5hpRkH").withEmoji(Emoji.fromCustom("chat", Long.parseLong("880875728915275786"), true)));
+            buttons.add(Button.secondary("welcome.question", "Stelle eine Frage!").withUrl(Channels.TICKETPANEL.getGuildChannel(Roonie.mainGuild).getJumpUrl()).withEmoji(Emoji.fromFormatted("❓")));
+            buttons.add(Button.success("welcome.features", "Schau dir unsere Features an!").withEmoji(Emoji.fromCustom("level", 909085962934562896L, false)));
             try {
                 event.getUser().openPrivateChannel().complete().sendMessageEmbeds(bannerEmbed.build(), mainEmbed.build()).setActionRow(buttons).queue();
             } catch (Exception e) {
@@ -49,17 +52,9 @@ public class WelcomeListener extends ListenerAdapter {
 
     public void onButtonInteraction (ButtonInteractionEvent event) {
         if (!event.isFromGuild()) {
-            EmbedBuilder mainEmbed;
             switch (event.getButton().getId()) {
-                case "welcome.visit":
-                    //link
-                    break;
                 case "welcome.features":
-                    mainEmbed = new EmbedBuilder();
-                    mainEmbed.setColor(0x28346d);
-                    mainEmbed.setTitle("Unsere Features");
-                    break;
-                case "welcome.question":
+                    event.replyEmbeds(DefaultMessage.error("IN ARBET", "Diese Funktion ist zurzeit in Arbeit!")).setEphemeral(true).queue();
                     break;
             }
         }
