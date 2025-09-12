ebedf68fd6697ed460779b4e37a6b1455863d2cf
splayfery <splayfery@gmail.com>
2025-09-12 21:57:55 +0200

    Create java spring project structure

---
+++ src/main/java/de/splayfunityde/roonie/RoonieApplication.java
@@ -0,0 +1,13 @@
+package de.splayfunityde.roonie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoonieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoonieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/splayfunityde/roonie/RoonieApplicationTests.java
@@ -0,0 +1,13 @@
+package de.splayfunityde.roonie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoonieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
