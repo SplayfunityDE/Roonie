58e44c45455e0e3766b468694d4711351f0a0bcf
splayfery <splayfery@gmail.com>
2025-09-22 00:50:21 +0200

    Implement rest controller

---
+++ src/main/java/de/splayfer/roonie/modules/ticket/TicketController.java
@@ -0,0 +1,29 @@
+package de.splayfer.roonie.modules.ticket;
+
+import de.splayfer.roonie.Roonie;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/ticket")
+public class TicketController {
+
+    @DeleteMapping("/{id}")
+    public void closeTicket(@PathVariable String id, @RequestBody String reason) {
+        Ticket.getFromChannel(id).close(reason);
+    }
+
+    @PutMapping("/{id}")
+    public void updateTicket(@PathVariable String id, @RequestBody Map<String, String> body) {
+        Ticket ticket = Ticket.getFromChannel(id);
+        if (body.containsKey("supporter")) {
+            if (body.get("supporter") != "") {
+                ticket.setSupporter(Roonie.mainGuild.getMemberById(body.get("supporter")));
+            } else {
+                ticket.setSupporter(null);
+            }
+        }
+        ticket.updateMongoDB();
+    }
+}
