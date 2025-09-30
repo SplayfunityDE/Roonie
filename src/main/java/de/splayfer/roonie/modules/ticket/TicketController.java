94f941c0ee9cf8076a181cf52b6d9f02e9e107be
splayfery <splayfery@gmail.com>
2025-09-30 18:48:31 +0200

    debug lol

--- src/main/java/de/splayfer/roonie/modules/ticket/TicketController.java
+++ src/main/java/de/splayfer/roonie/modules/ticket/TicketController.java
@@ -16,6 +16,7 @@ public class TicketController {
 
     @PutMapping("/{id}")
     public void updateTicket(@PathVariable String id, @RequestBody Map<String, String> body) {
+        System.out.println(body.toString());
         Ticket ticket = Ticket.getFromChannel(id);
         if (body.containsKey("supporter")) {
             if (body.get("supporter") != "") {
